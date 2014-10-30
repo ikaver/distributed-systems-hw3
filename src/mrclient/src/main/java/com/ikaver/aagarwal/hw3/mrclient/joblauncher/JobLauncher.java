38bdbcc2a1b82803f4e4be007d79bd3e93feb361
Iosef Kaver <iosef.kaver@gmail.com>
2014-10-29 21:43:09 -0400

    Added job launcher implementation

--- src/mrclient/src/main/java/com/ikaver/aagarwal/hw3/mrclient/joblauncher/JobLauncher.java
+++ src/mrclient/src/main/java/com/ikaver/aagarwal/hw3/mrclient/joblauncher/JobLauncher.java
@@ -1,16 +1,42 @@
 package com.ikaver.aagarwal.hw3.mrclient.joblauncher;
 
+import java.rmi.RemoteException;
+
+import org.apache.log4j.Logger;
+
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
+import com.google.inject.Inject;
 import com.ikaver.aagarwal.hw3.common.config.Job;
+import com.ikaver.aagarwal.hw3.common.config.JobInfo;
+import com.ikaver.aagarwal.hw3.common.master.IJobManager;
 
 public class JobLauncher {
   
-  public void launchJob(Job job) {
-
+  private static final Logger LOG = Logger.getLogger(JobLauncher.class);
+  private IJobManagerFactory factory;
+ 
+  @Inject
+  public JobLauncher(IJobManagerFactory factory) {
+    this.factory = factory;
+  }
+  
+  public JobInfo launchJob(Job job) {
+    IJobManager manager = this.factory.jobManagerFromJob(job);
+    if(manager == null) return null;
+    
+    JobInfo jobInfo = null;
+    try {
+      jobInfo = manager.createJob(job);
+    } catch (RemoteException e) {
+      LOG.info("Failed launching job", e);
+    }
+    
+    return jobInfo;
   }
   
   public static void main(String [] args) {
+    JobLauncherConfig.initialize();
     JobLauncherSettings settings = new JobLauncherSettings();
     JCommander argsParser = new JCommander(settings);
     try {
