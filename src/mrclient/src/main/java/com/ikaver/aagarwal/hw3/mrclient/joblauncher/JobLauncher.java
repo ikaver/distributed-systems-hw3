c3f26c8e5c37eec82eadd98d92406ea15d12dbbd
Iosef Kaver <iosef.kaver@gmail.com>
2014-10-29 22:47:39 -0400

    Started work on the JobFromJSONCreator

--- src/mrclient/src/main/java/com/ikaver/aagarwal/hw3/mrclient/joblauncher/JobLauncher.java
+++ src/mrclient/src/main/java/com/ikaver/aagarwal/hw3/mrclient/joblauncher/JobLauncher.java
@@ -1,13 +1,20 @@
 package com.ikaver.aagarwal.hw3.mrclient.joblauncher;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.rmi.RemoteException;
 
 import org.apache.log4j.Logger;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
+import com.google.inject.Guice;
 import com.google.inject.Inject;
+import com.google.inject.Injector;
 import com.ikaver.aagarwal.hw3.common.config.Job;
+import com.ikaver.aagarwal.hw3.common.config.JobFromJSONCreator;
 import com.ikaver.aagarwal.hw3.common.config.JobInfo;
 import com.ikaver.aagarwal.hw3.common.master.IJobManager;
 
@@ -35,8 +42,7 @@ public class JobLauncher {
     return jobInfo;
   }
   
-  public static void main(String [] args) {
-    JobLauncherConfig.initialize();
+  public static void main(String [] args) throws UnsupportedEncodingException, IOException {
     JobLauncherSettings settings = new JobLauncherSettings();
     JCommander argsParser = new JCommander(settings);
     try {
@@ -46,9 +52,12 @@ public class JobLauncher {
       argsParser.usage();
       System.exit(-1);
     }
-    
+    Injector injector = Guice.createInjector(new JobLauncherModule());
     String filePath = settings.getConfigurationFilePath();
-    
+    Job job = JobFromJSONCreator.createJobFromJSONFile(new File(filePath));
+    JobLauncher launcher = injector.getInstance(JobLauncher.class);
+    JobInfo info = launcher.launchJob(job);
+    System.out.println("Got job info: " + info);
   }
   
 }
