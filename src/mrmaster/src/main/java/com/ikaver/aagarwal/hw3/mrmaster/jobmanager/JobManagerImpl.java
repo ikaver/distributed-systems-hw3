55f3fa6b68efeb75b630e699fa30f24173b28581
Iosef Kaver <iosef.kaver@gmail.com>
2014-11-01 12:49:12 -0600

    Implemented mock JobManager class. Added JobManagerImpl class as well

---
+++ src/mrmaster/src/main/java/com/ikaver/aagarwal/hw3/mrmaster/jobmanager/JobManagerImpl.java
@@ -0,0 +1,32 @@
+package com.ikaver.aagarwal.hw3.mrmaster.jobmanager;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
+import com.ikaver.aagarwal.hw3.common.config.Job;
+import com.ikaver.aagarwal.hw3.common.config.JobInfo;
+import com.ikaver.aagarwal.hw3.common.master.IJobManager;
+
+public class JobManagerImpl implements IJobManager {
+
+  public JobInfo createJob(Job job) throws RemoteException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public List<JobInfo> listJobs() throws RemoteException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public boolean terminate(int jobID) throws RemoteException {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  public JobInfo getJobInfo(int jobID) throws RemoteException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
---
+++ src/mrmaster/src/main/java/com/ikaver/aagarwal/hw3/mrmaster/jobmanager/JobManagerMockImpl.java
@@ -0,0 +1,47 @@
+package com.ikaver.aagarwal.hw3.mrmaster.jobmanager;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.ikaver.aagarwal.hw3.common.config.Job;
+import com.ikaver.aagarwal.hw3.common.config.JobInfo;
+import com.ikaver.aagarwal.hw3.common.master.IJobManager;
+
+public class JobManagerMockImpl implements IJobManager {
+  
+  private Map<Integer, JobInfo> jobIDToJobInfo;
+  private int currentId;
+  
+  public JobManagerMockImpl() {
+    this.jobIDToJobInfo = new HashMap<Integer, JobInfo>();
+    this.currentId = 0;
+  }
+
+  public JobInfo createJob(Job job) throws RemoteException {
+    JobInfo jobInfo = new JobInfo(this.getNewID(), job);
+    jobIDToJobInfo.put(jobInfo.getJobID(), jobInfo);
+    return jobInfo;
+  }
+
+  public List<JobInfo> listJobs() throws RemoteException {
+    return new ArrayList<JobInfo>(this.jobIDToJobInfo.values());
+  }
+
+  public boolean terminate(int jobID) throws RemoteException {
+    boolean success = this.jobIDToJobInfo.containsKey(jobID);
+    this.jobIDToJobInfo.remove(jobID);
+    return success;
+  }
+
+  public JobInfo getJobInfo(int jobID) throws RemoteException {
+    return this.jobIDToJobInfo.get(jobID);
+  }
+
+  private synchronized int getNewID() {
+    return this.currentId++;
+  }
+  
+}
