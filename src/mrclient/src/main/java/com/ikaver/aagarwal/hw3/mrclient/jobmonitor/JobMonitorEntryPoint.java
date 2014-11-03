efc1551c58a97ad1d66052d3ca2f3d3d17167813
Iosef Kaver <iosef.kaver@gmail.com>
2014-11-03 00:46:57 -0500

    Adding program parameters to job monitor

--- src/mrclient/src/main/java/com/ikaver/aagarwal/hw3/mrclient/jobmonitor/JobMonitorEntryPoint.java
+++ src/mrclient/src/main/java/com/ikaver/aagarwal/hw3/mrclient/jobmonitor/JobMonitorEntryPoint.java
@@ -1,13 +1,24 @@
 package com.ikaver.aagarwal.hw3.mrclient.jobmonitor;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class JobMonitorEntryPoint {
-  
 
   public static void main(String [] args) {
-    Injector injector = Guice.createInjector(new JobMonitorModule("localhost", 3000));
+    JobMonitorSettings settings = new JobMonitorSettings();
+    JCommander argsParser = new JCommander(settings);
+    try {
+      argsParser.parse(args);
+    }
+    catch (ParameterException ex) {
+      argsParser.usage();
+    }
+    
+    Injector injector = Guice.createInjector(
+        new JobMonitorModule(settings.getMasterHost(), settings.getMasterPort()));
     JobMonitorController controller = injector.getInstance(JobMonitorController.class);
     controller.start();
   }
