f49d7df1c6bedca08dc7a70521d4c66d3190b1b8
Iosef Kaver <iosef.kaver@gmail.com>
2014-11-07 22:51:17 -0500

    Adding scheduler impl (not implemented yet)

---
+++ src/mrmaster/src/main/java/com/ikaver/aagarwal/hw3/mrmaster/scheduler/IMRSchedulerImpl.java
@@ -0,0 +1,21 @@
+package com.ikaver.aagarwal.hw3.mrmaster.scheduler;
+
+import java.util.Set;
+
+import com.ikaver.aagarwal.hw3.common.workers.MapWorkDescription;
+import com.ikaver.aagarwal.hw3.common.workers.ReduceWorkDescription;
+
+public class IMRSchedulerImpl implements IMRScheduler {
+
+  public Set<MapperWorkerInfo> runMappersForWork(Set<MapWorkDescription> work) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Set<ReducerWorkerInfo> runReducersForWork(
+      Set<ReduceWorkDescription> work) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
