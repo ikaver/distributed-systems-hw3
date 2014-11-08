2f3488be38813d2d734f1a24e9eaf9c8b606afab
Iosef Kaver <iosef.kaver@gmail.com>
2014-11-08 03:48:38 -0500

    Adding DFS factory

---
+++ src/mrdfs/src/main/java/com/ikaver/aagarwal/hw3/mrdfs/master/DFSFactory.java
@@ -0,0 +1,38 @@
+package com.ikaver.aagarwal.hw3.mrdfs.master;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import com.ikaver.aagarwal.hw3.common.definitions.Definitions;
+import com.ikaver.aagarwal.hw3.common.dfs.IDFS;
+import com.ikaver.aagarwal.hw3.common.util.SocketAddress;
+
+public class DFSFactory {
+  
+  private static final Logger LOG = LogManager.getLogger(DFSFactory.class);
+
+  public static IDFS dataNodeFromSocketAddress(SocketAddress addr) {
+    if(addr == null) return null;
+    String url = String.format(
+        "//%s:%d/%s", 
+        addr.getHostname(), 
+        addr.getPort(),
+        Definitions.DFS_SERVICE
+        );
+    try {
+      return (IDFS) Naming.lookup (url);
+    } catch (MalformedURLException e) {
+      LOG.info("Bad URL", e);
+    } catch (RemoteException e) {
+      LOG.info("Remote connection refused to url "+ url, e);
+    } catch (NotBoundException e) {
+      LOG.info("Not bound", e);
+    }
+    return null;
+  }
+}
