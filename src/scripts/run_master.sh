22be5d0c409cd35c972c7d375fd83777fbfeabac
Ankit Agarwal <ankit1990@gmail.com>
2014-11-15 16:28:16 -0500

    Removed absolute paths.

--- src/scripts/run_master.sh
+++ src/scripts/run_master.sh
@@ -1,3 +1,3 @@
 # This file should contain the command line of the binary which should start the master.
 # Port at which the master should run.
-nohup java -jar /tmp/mrmaster-1.0-SNAPSHOT-jar-with-dependencies.jar -port $1 -slaves $2> /tmp/master.stdout 2> /tmp/master.stderr  &
+nohup java -jar mrmaster-1.0-SNAPSHOT-jar-with-dependencies.jar -port $1 -slaves $2> /tmp/master.stdout 2> /tmp/master.stderr  &
--- src/scripts/run_slave.sh
+++ src/scripts/run_slave.sh
@@ -1,3 +1,3 @@
 # This file should contain the command line for starting MR slave process.
 # Usage $1=port $2=master_ip $3=master_port. Port is the port at which the service runs.
-nohup java -jar /tmp/mrnodemanager-1.0-SNAPSHOT-jar-with-dependencies.jar -port $1 -masterIP $2 -masterPort $3> /tmp/slave.stdout 2> /tmp/slave.stderr  &
+nohup java -jar mrnodemanager-1.0-SNAPSHOT-jar-with-dependencies.jar -port $1 -masterIP $2 -masterPort $3> /tmp/slave.stdout 2> /tmp/slave.stderr  &
