58d3c7ff89a0f32e5ab34e83440e7e607420383d
Ankit Agarwal <ankit1990@gmail.com>
2014-11-19 18:54:53 -0500

    Added code for buggy word count mapper. This will be used to demonstrate fault tolerance in our system.

---
+++ src/common/src/main/java/com/ikaver/aagarwal/hw3/common/examples/BuggyWordCountMapper.java
@@ -0,0 +1,26 @@
+package com.ikaver.aagarwal.hw3.common.examples;
+
+import com.ikaver.aagarwal.hw3.common.mrcollector.ICollector;
+
+/**
+ * A buggy word count mapper which demonstrates that our system is tolerant
+ * to errors in user code.
+ */
+public class BuggyWordCountMapper {
+	
+	public void map(String record, ICollector collector) {
+		String cleanRecord = record.trim();
+		collector.collect(cleanRecord, "1");	
+		doEvilStuff();
+		
+	}
+	
+	/**
+	 * Although this is not a "pure" JVM crash, but nevertheless is enough
+	 * to demonstrate the functionalities of our code.
+	 */
+	private void doEvilStuff() {
+		System.exit(1);
+	}
+
+}
