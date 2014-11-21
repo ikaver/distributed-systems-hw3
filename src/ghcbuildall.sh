7f3af0e066ea5b7b5b08e9bae972a5b7ee8f596f
Iosef Kaver <iosef.kaver@gmail.com>
2014-11-21 02:56:40 -0500

    localbuildall and ghcbuildall now also build needed python libs

--- src/ghcbuildall.sh
+++ src/ghcbuildall.sh
@@ -23,3 +23,22 @@ cd ..
 cd mrreduce
 /usr/local/netbeans-7.2.1/java/maven/bin/mvn clean
 /usr/local/netbeans-7.2.1/java/maven/bin/mvn assembly:assembly -DdescriptorId=jar-with-dependencies
+cd ..;
+cd scripts;
+cd ecdsa-0.11/ ;
+python setup.py build ;
+cp -r build/lib/ecdsa ../ ;
+cd .. ;
+cd pycrypto-2.6.1/ ;
+python setup.py build ; 
+cd build;
+cd lib* ;
+cp -r Crypto ../../../
+cd .. ;
+cd .. ;
+cd .. ;
+cd paramiko_dir ;
+python setup.py build ; 
+cp -r build/lib/paramiko ../ ; 
+cd ..;
+cd ..;
--- src/localbuildall.sh
+++ src/localbuildall.sh
@@ -23,3 +23,22 @@ cd ..
 cd mrreduce
 mvn clean
 mvn assembly:assembly -DdescriptorId=jar-with-dependencies
+cd ..;
+cd scripts;
+cd ecdsa-0.11/ ;
+python setup.py build ;
+cp -r build/lib/ecdsa ../ ;
+cd .. ;
+cd pycrypto-2.6.1/ ;
+python setup.py build ; 
+cd build;
+cd lib* ;
+cp -r Crypto ../../../
+cd .. ;
+cd .. ;
+cd .. ;
+cd paramiko_dir ;
+python setup.py build ; 
+cp -r build/lib/paramiko ../ ; 
+cd .. ;
+cd .. ;
