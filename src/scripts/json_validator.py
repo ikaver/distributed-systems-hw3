cad3b233e9d7556e0131c0960d6e7323d0038ad9
Ankit Agarwal <ankit1990@gmail.com>
2014-11-20 20:47:24 -0500

    Adding python script for json validation.

---
+++ src/scripts/json_validator.py
@@ -0,0 +1,72 @@
+import argparse;
+import json;
+
+CONFIG = "config";
+
+def setup_args():
+  parser = argparse.ArgumentParser();
+  parser.add_argument("--json_file", type=str, required=True);
+  return parser.parse_args();
+
+def validate_participants(nodes):
+  if len(nodes) < 1:
+    print "Atleast one node must be specified";
+    return False;
+  
+  for node in nodes:
+    if "ip" not in node:
+      print "IP should be specified for each participant";
+      return False;
+    if "port" not in node or not isinstance(node["port"], int) or node["port"] < 0:
+      print "Port for each participant must be greater than zero.";
+      return False;
+  return True;
+    
+def validate_json(content):
+  d = json.loads(content);
+  
+  if len(d) != 1 and CONFIG not in d:
+    print "Only and only one configuration should be specified in the json";
+    return False;
+  
+  integer_children = [
+        "master-port",
+        "max-retries-before-job-failure",
+        "time-to-check-for-job-state",
+        "time-to-check-for-nodes-state",
+        "time-to-check-for-data-nodes-state",
+        "workers-per-node",
+        "replication-factor",
+        "chunk-size-in-MB",
+        "max-dfs-read-retries"];
+
+  if "master-host" not in d[CONFIG]:
+    print "Master host should be specified";
+    return False;
+ 
+  for child in integer_children:
+    if child not in d[CONFIG]:
+      print "{0} not specified under config".format(child);
+      return False;
+      value = d[CONFIG][child];
+      if not isinstance(int, value) or value <= 0:
+        print "{0} should be an integer > 0".format(child);
+        return False;
+   
+  return validate_participants(d[CONFIG]["participants"]);
+
+def read_file(filename):
+  f = open(filename);
+  contents = f.read();
+  return contents.replace('\n','');
+
+def main():
+  args = setup_args();
+  if (validate_json(read_file(args.json_file))):
+    print "JSON validated successfully :)";
+  else:
+    print "JSON parsing was not successful. :(";
+
+if __name__ == "__main__":
+  main();
+ 
